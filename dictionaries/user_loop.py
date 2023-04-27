4eb15270c225df9d7a9aaa78eee41e0ea83e2666
sudarsana kasireddy <sudaredd@gmail.com>
2023-04-26 20:05:03 -0400

    dictionary samples

--- dictionaries/user_loop.py
+++ dictionaries/user_loop.py
@@ -4,5 +4,31 @@ user_0 = {
 'last': 'fermi',
 }
 
-for key, val in user_0.items():
-    print("key "+key + " and value "+val)
+# for key, val in user_0.items():
+#     print("key "+key + " and value "+val)
+
+for val in user_0.values():
+    print ("value is ", val)
+
+# Initialize an empty dictionary
+my_dict = {}
+
+# Add key-value pairs to the dictionary only if key doesn't exist
+my_dict.setdefault('key1', set())
+my_dict.setdefault('key2', [])
+
+# Append elements to the list corresponding to the keys
+my_dict['key1'].add('value1')
+my_dict['key1'].add('value1')
+my_dict['key1'].add('value2')
+my_dict.setdefault('key1', set()).add('valuenkk')
+
+
+my_dict['key2'].append('value3')
+my_dict['key2'].append('value4')
+
+# Add element to a non-existing key
+my_dict.setdefault('key3', []).append('value5')
+
+# Print the dictionary
+print(my_dict)
